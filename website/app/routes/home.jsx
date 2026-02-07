3c2418e0dbec58ab616d96550940450e6d50d9d3
Junior Jimenez <klys2007@gmail.com>
2026-02-07 16:56:46 -0500

    transformed to JSX react

---
+++ website/app/routes/home.jsx
@@ -0,0 +1,13 @@
+import { Welcome } from "../welcome/welcome";
+import MainPage from "../home/home";
+
+export function meta() {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <MainPage />;
+}
